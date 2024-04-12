313b463671fed87848f022ebd7064130fbcd8a5a
Alexandre Poupeau <ale.poupeau@gmail.com>
2024-04-11 18:00:27 -0600

    feat: func to display image + lines

--- src/utils/img_tools.py
+++ src/utils/img_tools.py
@@ -3,9 +3,10 @@ File utils for image manipulation
 """
 
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
-def show_rgb_image(image, title=None, conversion=cv2.COLOR_BGR2RGB):
+def show_rgb_image(image: np.array, title=None, conversion=cv2.COLOR_BGR2RGB):
     # Converts from one colour space to the other. this is needed as RGB
     # is not the default colour space for OpenCV
     image = cv2.cvtColor(image, conversion)
@@ -22,3 +23,21 @@ def show_rgb_image(image, title=None, conversion=cv2.COLOR_BGR2RGB):
         plt.title(title)
 
     plt.show()
+    
+def show_image_and_lines(
+        image: np.array, 
+        lines: np.array,
+        colors_lines: np.array=None,
+        title=None, 
+        conversion=cv2.COLOR_BGR2RGB,
+        default_color = (0, 255, 255)
+    ):
+    if colors_lines is None:
+        colors_lines = [default_color for i in range(len(lines))]
+    assert len(lines) == len(colors_lines)
+    
+    img_copy = image.copy()
+    img_copy = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2BGR)
+    for line, color in zip(lines, colors_lines):
+        cv2.line(img_copy, line[0], line[1], color, 3, cv2.LINE_AA)
+    show_rgb_image(image=img_copy, title=title, conversion=conversion)
