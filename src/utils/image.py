a599dba3a3a28b7230045666855c5821cb5c476e
Alexandre Poupeau <ale.poupeau@gmail.com>
2024-04-12 19:11:50 -0600

    chore: rename image utility module

--- src/utils/image.py
+++ src/utils/image.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def show_rgb_image(image: np.array, title=None, conversion=cv2.COLOR_BGR2RGB):
+def show_image(image: np.array, title=None, conversion=cv2.COLOR_BGR2RGB):
     # Converts from one colour space to the other. this is needed as RGB
     # is not the default colour space for OpenCV
     image = cv2.cvtColor(image, conversion)
@@ -40,4 +40,4 @@ def show_image_and_lines(
     img_copy = cv2.cvtColor(img_copy, cv2.COLOR_GRAY2BGR)
     for line, color in zip(lines, colors_lines):
         cv2.line(img_copy, line[0], line[1], color, 3, cv2.LINE_AA)
-    show_rgb_image(image=img_copy, title=title, conversion=conversion)
+    show_image(image=img_copy, title=title, conversion=conversion)
