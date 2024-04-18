b0e5a1de14bcfbef98a46ca2fa8f2a2af3e79129
Alexandre Poupeau <ale.poupeau@gmail.com>
2024-04-17 18:03:50 -0600

    feat: create Image class

--- src/utils/image.py
+++ src/utils/image.py
@@ -6,8 +6,18 @@ import cv2
 import scipy.ndimage
 import numpy as np
 import matplotlib.pyplot as plt
-from src.geometry.segment import Segment
+import src
+import src.geometry as geo
 from src.utils.geometry import compute_slope_angle
+
+class Image:
+    
+    def __init__(self, image: np.ndarray) -> None:
+        self.asarray = image.copy()
+    
+    @property
+    def center(self):
+        return (np.array([self.asarray.shape[1], self.asarray.shape[0]]) / 2).astype(int)
 
 def show_image(image: np.ndarray, title=None, conversion=cv2.COLOR_BGR2RGB):
     # Converts from one colour space to the other. this is needed as RGB
@@ -38,7 +48,7 @@ def show_image_and_lines(
         conversion=cv2.COLOR_BGR2RGB,
         default_color = (0, 255, 255)
     ):
-    lines = lines.astype(int)
+    lines = np.asarray(lines).astype(int)
     if colors_lines is None:
         colors_lines = [default_color for i in range(len(lines))]
     assert len(lines) == len(colors_lines)
@@ -92,7 +102,7 @@ def center_image_to_point(image: np.ndarray, point: np.ndarray, mode: str="const
     center_img_vector = (np.array([image.shape[1], image.shape[0]]) / 2).astype(int)
     shift_vector = center_img_vector - point
     img = scipy.ndimage.shift(input=image, shift=np.roll(shift_vector, 1), mode=mode)
-    return img
+    return img, shift_vector
     
 def center_image_to_line(image: np.ndarray, line: np.ndarray, mode: str="constant"):
     """Shift the image so that the line middle point ends up in the middle of the new image
@@ -127,6 +137,12 @@ def resize_image(image: np.ndarray, scale_percent: float):
     dim = (width, height)
     img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
     return img
+
+def crop_image_horizontal(image: np.ndarray, x0, y0, x1, y1):
+    image = image.copy()
+    img_cropped = image[x0:x1, y0:y1]
+    anchor = np.array([y0, x0])
+    return img_cropped, anchor
     
     
 def crop_image_around_line_horizontal(
@@ -141,11 +157,11 @@ def crop_image_around_line_horizontal(
     _img = image.copy()
     
     # center the image based on the middle of the line
-    center_pt_img = (np.array([_img.shape[1], _img.shape[0]]) / 2).astype(int)
-    _img = center_image_to_line(image=_img, line=line)
+    _img, translation_vector = center_image_to_line(image=_img, line=line)
     
     if show_translation:
-        middle_pt_line = (np.sum(line, axis=0) / 2).astype(int)
+        center_pt_img = Image(_img).center
+        middle_pt_line = geo.Segment(line).centroid
         _img2 = cv2.cvtColor(_img, cv2.COLOR_GRAY2RGB)
         cv2.circle(_img2, center=middle_pt_line, radius=2, color=(0, 0, 255), thickness=5)
         cv2.circle(_img2, center=center_pt_img, radius=2, color=(0, 0, 255), thickness=5)
@@ -157,13 +173,13 @@ def crop_image_around_line_horizontal(
     _img = scipy.ndimage.rotate(input=_img, angle=angle_degree, reshape=True)
 
     # crop the image
-    center_pt_img = (np.array([_img.shape[1], _img.shape[0]]) / 2).astype(int)
+    center_pt_img = Image(_img).center
     if width_crop_rect is None:
         # default the width for crop to be a bit more than line length
-        width_crop_rect = Segment(line).length + extra_length_default_width
+        width_crop_rect = geo.Segment(line).length + extra_length_default_width
     x0, y0 = int(center_pt_img[1] - heigth_crop_rect / 2), int(center_pt_img[0] - width_crop_rect / 2)
     x1, y1 = int(center_pt_img[1] + heigth_crop_rect / 2), int(center_pt_img[0] + width_crop_rect / 2)
-    _img_cropped = _img[x0:x1, y0:y1]
+    _img_cropped, anchor = crop_image_horizontal(_img, x0, y0, x1, y1)
 
     _img_cropped_resized = resize_image(image=_img_cropped, scale_percent=resize_scale_percent)
-    return _img_cropped_resized
+    return _img_cropped_resized, anchor, translation_vector, angle_degree
