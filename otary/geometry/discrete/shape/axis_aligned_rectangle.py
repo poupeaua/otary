2238e674cc2154e83928f13f4ff948b3c7c92ff9
Alexandre Poupeau <ale.poupeau@gmail.com>
2025-08-23 13:46:04 +0200

    feat: add new properties and unit tests to axis aligned rect

--- otary/geometry/discrete/shape/axis_aligned_rectangle.py
+++ otary/geometry/discrete/shape/axis_aligned_rectangle.py
@@ -3,9 +3,9 @@ Axis Aligned Rectangle python file
 """
 
 from __future__ import annotations
+from typing import Optional
 
 import pymupdf
-
 from numpy.typing import NDArray
 
 from otary.geometry.discrete.shape.rectangle import Rectangle
@@ -14,15 +14,20 @@ from otary.geometry.discrete.shape.rectangle import Rectangle
 class AxisAlignedRectangle(Rectangle):
     """
     Axis Aligned Rectangle class that inherits from Rectangle.
-    It defines a rectangle that is axis-aligned, meaning its sides are parallel
-    to the X and Y axes.
+    It defines a rectangle that is axis-aligned, meaning:
+     - its sides are parallel to the X and Y axes
+     - the first point is the top-left point
+     - the points are ordered clockwise
     """
 
     def __init__(self, points: NDArray | list, is_cast_int: bool = False) -> None:
         super().__init__(points=points, is_cast_int=is_cast_int)
 
         if not self.is_axis_aligned:
-            raise ValueError("The rectangle is not axis aligned")
+            raise ValueError(
+                "Trying to create an AxisAlignedRectangle from a non-axis-aligned " \
+                "rectangle. Please check the input points."
+            )
 
     @classmethod
     def from_rectangle(cls, rectangle: Rectangle) -> AxisAlignedRectangle:
@@ -55,6 +60,42 @@ class AxisAlignedRectangle(Rectangle):
         return cls.from_rectangle(
             super().from_center(center, width, height, angle=0, is_cast_int=is_cast_int)
         )
+    
+    @property
+    def height(self) -> float:
+        """Get the height of the AxisAlignedRectangle
+
+        Returns:
+            float: height of the rectangle
+        """
+        return self.ymax - self.ymin
+    
+    @property
+    def width(self) -> float:
+        """Get the width of the AxisAlignedRectangle
+
+        Returns:
+            float: width of the rectangle
+        """
+        return self.xmax - self.xmin
+    
+    @property
+    def area(self) -> float:
+        """Get the area of the AxisAlignedRectangle
+
+        Returns:
+            float: area of the rectangle
+        """
+        return self.width * self.height
+    
+    @property
+    def perimeter(self) -> float:
+        """Get the perimeter of the AxisAlignedRectangle
+
+        Returns:
+            float: perimeter of the rectangle
+        """
+        return 2 * (self.width + self.height)
 
     @property
     def as_pymupdf_rect(self) -> pymupdf.Rect:
@@ -67,3 +108,45 @@ class AxisAlignedRectangle(Rectangle):
             pymupdf.Rect: pymupdf axis-aligned Rect object
         """
         return pymupdf.Rect(x0=self.xmin, y0=self.ymin, x1=self.xmax, y1=self.ymax)
+    
+    @property
+    def rotated90(self) -> AxisAlignedRectangle:
+        """Get the unique other related AxisAlignedRectangle that is the same one
+        but rotated 90 degrees around its center.
+
+        Returns:
+            AxisAlignedRectangle: new AxisAlignedRectangle object
+        """
+        return AxisAlignedRectangle.from_center(
+            center=self.centroid, width=self.height, height=self.width
+        )
+
+    def rotate(
+        self,
+        angle: float,
+        is_degree: bool = False,
+        is_clockwise: bool = True,
+        pivot: Optional[NDArray] = None,
+    ) -> Rectangle:
+        """Rotate the AxisAlignedRectangle.
+        The result is a Rectangle object since the result is not
+        guaranteed to be axis-aligned anymore.
+
+        Args:
+            angle (float): rotation angle
+            is_degree (bool, optional): whether the angle is in degree or radian.
+                Defaults to False which means radians.
+            is_clockwise (bool, optional): whether the rotation is clockwise or
+                counter-clockwise. Defaults to True.
+            pivot (NDArray, optional): pivot point.
+                Defaults to None which means that by default the centroid point of
+                the shape is taken as the pivot point.
+
+        Returns:
+            Rectangle: object resulting from the rotation
+        """
+        super().rotate(
+            angle=angle, is_degree=is_degree, is_clockwise=is_clockwise, pivot=pivot
+        )
+
+        return Rectangle(points=self.points)
--- tests/unit/geometry/discrete/shape/test_axis_aligned_rectangle.py
+++ tests/unit/geometry/discrete/shape/test_axis_aligned_rectangle.py
@@ -3,12 +3,90 @@ Tests for the AxisAlignedRectangle class
 """
 
 import pymupdf
+import pytest
 
 from otary.geometry import AxisAlignedRectangle
 
-class TestRectanglePyMuRect:
 
-    def test_as_pymupdf_rect_axis_aligned(self):
+class TestAxisAlignedRectangleCreation:
+
+    def test_create_axis_aligned_rectangle_base(self):
+        # Create an axis-aligned rectangle
+        rect = AxisAlignedRectangle.from_topleft(topleft=[0, 0], width=2, height=4)
+
+        # Assert the rectangle has correct coordinates
+        assert rect.xmin == 0
+        assert rect.ymin == 0
+        assert rect.xmax == 2
+        assert rect.ymax == 4
+
+        # assert first point is top-left and points are ordered clockwise
+        assert (rect.points[0] == [0, 0]).all()
+        assert (rect.points[1] == [2, 0]).all()
+        assert (rect.points[2] == [2, 4]).all()
+        assert (rect.points[3] == [0, 4]).all()
+
+    def test_create_axis_aligned_rectangle_from_center(self):
+        # Create an axis-aligned rectangle from center
+        rect = AxisAlignedRectangle.from_center(center=[5, 5], width=4, height=2)
+
+        # Assert the rectangle has correct coordinates
+        assert rect.xmin == 3
+        assert rect.ymin == 4
+        assert rect.xmax == 7
+        assert rect.ymax == 6
+
+        # assert first point is top-left and points are ordered clockwise
+        assert (rect.points[0] == [3, 4]).all()
+        assert (rect.points[1] == [7, 4]).all()
+        assert (rect.points[2] == [7, 6]).all()
+        assert (rect.points[3] == [3, 6]).all()
+
+    def test_create_non_axis_aligned_rectangle_raises(self):
+        # Attempt to create a non-axis-aligned rectangle
+        with pytest.raises(ValueError):
+            AxisAlignedRectangle(
+                points=[[0, 0], [2, 1], [3, 3], [1, 2]]
+            )
+
+    def test_create_non_axis_aligned_rectangle_raises2(self):
+        # Attempt to create a non-axis-aligned rectangle
+        with pytest.raises(ValueError):
+            AxisAlignedRectangle(
+                points=[[0, 0], [5, 0], [5, 10], [-1, 10]]
+            )
+
+class TestAxisAlignedRectangleProperties:
+
+    def test_height_and_width_properties(self):
+        # Create an axis-aligned rectangle
+        rect = AxisAlignedRectangle.from_topleft(topleft=[0, 0], width=3, height=5)
+
+        # Assert the height and width properties are correct
+        assert rect.height == 5
+        assert rect.width == 3
+
+    def test_area_property(self):
+        # Create an axis-aligned rectangle
+        width = 3
+        height = 5
+        rect = AxisAlignedRectangle.from_topleft(topleft=[0, 0], width=3, height=5)
+
+        # Assert the area property is correct
+        assert rect.area == width * height
+
+    def test_perimeter_property(self):
+        # Create an axis-aligned rectangle
+        width = 3
+        height = 5
+        rect = AxisAlignedRectangle.from_topleft(topleft=[0, 0], width=width, height=height)
+
+        # Assert the perimeter property is correct
+        assert rect.perimeter == 2 * (width + height)
+
+class TestAxisAlignedRectanglePyMuRect:
+
+    def test_as_pymupdf_rect_base(self):
         # Create an axis-aligned rectangle
         rect = AxisAlignedRectangle.from_topleft(topleft=[0, 0], width=2, height=4)
         pymupdf_rect = rect.as_pymupdf_rect
@@ -19,3 +97,48 @@ class TestRectanglePyMuRect:
         assert pymupdf_rect.y0 == 0
         assert pymupdf_rect.x1 == 2
         assert pymupdf_rect.y1 == 4
+
+class TestAxisAlignedRectangleShift:
+
+    def test_shift_base(self):
+        # Create an axis-aligned rectangle
+        rect = AxisAlignedRectangle.from_topleft(topleft=[1, 1], width=3, height=2)
+
+        # Shift the rectangle by (2, 3)
+        rect.shift(vector=[2, 3])
+
+        # Assert the new coordinates are correct
+        assert rect.xmin == 3
+        assert rect.ymin == 4
+        assert rect.xmax == 6
+        assert rect.ymax == 6
+
+class TestAxisAlignedRectangleRotated90:
+
+    def test_rotated90_assert_axis_aligned(self):
+        # Create an axis-aligned rectangle
+        rect = AxisAlignedRectangle.from_topleft(topleft=[1, 1], width=3, height=2)
+
+        # Rotate the rectangle by 90 degrees
+        rect_rot = rect.rotated90
+        assert rect_rot.is_axis_aligned
+
+    def test_rotated90_assert_new_coords(self):
+        # Create an axis-aligned rectangle
+        rect = AxisAlignedRectangle.from_topleft(topleft=[1, 1], width=3, height=2)
+        rect_rot = rect.rotated90
+
+        # Assert the new coordinates are correct
+        assert rect_rot.width == rect.height
+        assert rect_rot.height == rect.width
+        assert rect_rot.centroid[0] == rect.centroid[0]
+        assert rect_rot.centroid[1] == rect.centroid[1]
+
+    def test_rotated90_assert_topleft_and_clockwise(self):
+        # Create an axis-aligned rectangle
+        rect = AxisAlignedRectangle.from_topleft(topleft=[1, 1], width=3, height=2)
+        rect_rot = rect.rotated90
+
+        # assert first point is top-left and points are ordered clockwise
+        assert (rect_rot.asarray[0] == [1.5, 0.5]).all()
+        assert rect_rot.is_clockwise(is_y_axis_down=True)
