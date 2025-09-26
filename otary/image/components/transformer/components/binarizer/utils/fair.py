d874489406312501caa6c23f8ee008819fced935
Alexandre Poupeau <ale.poupeau@gmail.com>
2025-09-26 12:14:49 +0200

    chore: optimize EM algo for fair binarization

--- otary/image/components/transformer/components/binarizer/utils/fair.py
+++ otary/image/components/transformer/components/binarizer/utils/fair.py
@@ -24,8 +24,8 @@ def responsability(x, mu_t, mu_b, var_t, var_b, omega):
 def expectation_maximization(
     img: NDArray,
     s: NDArray,
-    window_size: int = 101,
-    rel_tol: float = 1e-7,
+    window_size: int = 51,
+    rel_tol: float = 1e-2,
     max_iter: int = 100,
 ):
     """EM algorithm using both original image and edges image.
@@ -48,10 +48,10 @@ def expectation_maximization(
     x = patches[s[:, 0], s[:, 1]]  # shape (n_edges, window_size, window_size)
 
     # EM initialization - mu (mean), sigma (std), omega (mixing coefficient)
-    mu_t = np.min(x, axis=(1, 2))
-    mu_b = np.max(x, axis=(1, 2))
-    omega = np.random.random(size=n_edges)
-    gamma = np.random.random(size=(n_edges, window_size, window_size))
+    mu_t: NDArray = np.min(x, axis=(1, 2))[:, np.newaxis, np.newaxis]
+    mu_b: NDArray = np.mean(x, axis=(1, 2))[:, np.newaxis, np.newaxis] + mu_t / 2
+    omega = (np.random.random(size=(n_edges, 1, 1)) + 4) / 5
+    gamma = np.where(x < (mu_t + mu_b) / 2, 0.75, 0.25)
     var_t, var_b = 1, 1
 
     # perform the Expectation-Maximization loop
@@ -59,33 +59,20 @@ def expectation_maximization(
     i = 0
     while not is_converged and i < max_iter:
         # E-step
-        gamma = responsability(
-            x=x,
-            mu_t=mu_t[:, np.newaxis, np.newaxis],  # to allow broadcasting
-            mu_b=mu_b[:, np.newaxis, np.newaxis],
-            var_t=var_t,
-            var_b=var_b,
-            omega=omega[:, np.newaxis, np.newaxis],
-        )  # shape (n_edges, window_size, window_size)
-
-        print(gamma)
+        gamma = responsability(x, mu_t, mu_b, var_t, var_b, omega)
 
         # M-step or classic MLE
         # parameters with underscore are the updated ones
         ngamma = 1 - gamma
-        _omega = np.mean(gamma, axis=(1, 2))  # omega = avg of responsabilities = GAP
-        _mu_t = np.sum(gamma * x, axis=(1, 2)) / (np.sum(gamma, axis=(1, 2)) + 1e-9)
-        _mu_b = np.sum(ngamma * x, axis=(1, 2)) / (np.sum(ngamma, axis=(1, 2)) + 1e-9)
-        _var_t = np.sum(gamma * (x - mu_t[:, np.newaxis, np.newaxis]) ** 2) / (
-            np.sum(gamma) + 1e-9
+        _omega = np.mean(gamma, axis=(1, 2), keepdims=True)
+        _mu_t = np.sum(gamma * x, axis=(1, 2), keepdims=True) / (
+            np.sum(gamma, axis=(1, 2), keepdims=True) + 1e-9
         )
-        _var_b = np.sum(ngamma * (x - mu_b[:, np.newaxis, np.newaxis]) ** 2) / (
-            np.sum(ngamma) + 1e-9
+        _mu_b = np.sum(ngamma * x, axis=(1, 2), keepdims=True) / (
+            np.sum(ngamma, axis=(1, 2), keepdims=True) + 1e-9
         )
-
-        # avoid exploding values
-        # _var_t = np.clip(_var_t, a_min=1e-9, a_max=50)
-        # _var_b = np.clip(_var_b, a_min=1e-9, a_max=50)
+        _var_t = np.sum(gamma * (x - mu_t) ** 2) / (np.sum(gamma) + 1e-9)
+        _var_b = np.sum(ngamma * (x - mu_b) ** 2) / (np.sum(ngamma) + 1e-9)
 
         # check convergence
         is_converged = (
@@ -101,20 +88,32 @@ def expectation_maximization(
 
         # avoid infinite loop
         i += 1
-        print(i)
+
+    if i >= max_iter:
+        pass
+        # raise RuntimeError("EM algorithm did not converge for the FAIR method")
+
+    if mu_t.mean() > mu_b.mean():
+        # swap so mu_t always refers to darker (text)
+        mu_t, mu_b = mu_b, mu_t
+        var_t, var_b = var_b, var_t
+        omega = 1 - omega
+        gamma = 1 - gamma
 
     return {
         "x": x,
-        "mu_t": mu_t,
-        "mu_b": mu_b,
-        "sigma_t": var_t,
-        "sigma_b": var_b,
-        "omega": omega,
+        "mu_t": mu_t.squeeze(),
+        "mu_b": mu_b.squeeze(),
+        "var_t": var_t,
+        "var_b": var_b,
+        "omega": omega.squeeze(),
         "gamma": gamma,
     }
 
 
-def threshold_fair(img: NDArray, k: float = 1.0, alpha: float = 0.5, n: int = 3):
+def threshold_fair(
+    img: NDArray, k: float = 1.0, alpha: float = 0.5, n: int = 51, max_iter: int = 10
+):
     """FAIR thresholding method.
 
     Args:
@@ -123,6 +122,12 @@ def threshold_fair(img: NDArray, k: float = 1.0, alpha: float = 0.5, n: int = 3)
         alpha (float, optional): It defines the ratio to compute the lower threshold
             in the 1st step of the S-FAIR step. It is generally in [0.3, 0.5].
             Defaults to 0.4.
+        n (int, optional): window size for the EM algorithm and hence to cluster
+            background and foreground pixels around edge pixels.
+            This parameter is important as a higher value will make the method
+            more robust to noise but also more computationally expensive and slow.
+            Defaults to 51.
+        em_max_iter (int, optional): maximum number of iterations for the EM algorithm.
     """
     # Step 1 of S-FAIR - Text area detection
     gm = gradient_magnitude(img=img, window_size=3)
@@ -131,8 +136,10 @@ def threshold_fair(img: NDArray, k: float = 1.0, alpha: float = 0.5, n: int = 3)
     T_l = alpha * T_o  # T_l stands for lower threshold
     img_edged = cv2.Canny(image=img, threshold1=T_l, threshold2=T_u)  # values 0 or 255
     s = np.column_stack(np.where(img_edged == 255))  # edge pixel coordinates
+    thining = n // 2
+    s = s[::thining]  # downsample edge pixels to speed up EM
 
     # Step 2 of S-FAIR - Model estimation around edges
     # edges can be easily identified as they are 255 pixels in im_edges
-    s = expectation_maximization(img=img, s=s[:1])
+    s = expectation_maximization(img=img, s=s[:10000], window_size=n, max_iter=max_iter)
     return s
